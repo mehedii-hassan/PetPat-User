83b459bf1c87887fd5f9731fe9c2ebd47ac44ece
Mehedi Hassan <mehedi.github@gmail.com>
2023-08-02 14:29:05 +0600

    otp and splash screen design done

---
+++ app/src/main/java/com/example/petpatuser/ui/fragments/auth/SplashScreenFragment.kt
@@ -0,0 +1,23 @@
+package com.example.petpatuser.ui.fragments.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.petpatuser.R
+
+create an instance of this fragment.
+ */
+class SplashScreenFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+}
